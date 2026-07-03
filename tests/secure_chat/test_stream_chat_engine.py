2fe91d2a9f66b7b38d7ab49c011c5d7c8894fce8
Ajay Singala <ajay.singala@gmail.com>
2026-07-03 21:05:10 +0530

    better print statement

--- tests/secure_chat/test_stream_chat_engine.py
+++ tests/secure_chat/test_stream_chat_engine.py
@@ -3,6 +3,7 @@ from applications.secure_chat.chat_engine import ChatEngine
 engine = ChatEngine()
 generator = engine.stream_chat(
     "Explain Retrieval Augmented Generation."
+    # "Tell me a dad joke"
 )
 
 try:
@@ -18,4 +19,4 @@ except StopIteration as result:
 
 print()
 print()
-print(chat_result.total_tokens)
+print(f"Total Tokens: {chat_result.total_tokens}")
